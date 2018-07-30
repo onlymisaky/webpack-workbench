0bac7a6e70df060a8bc2da44df30636d610ce2d4
onlymisaky <onlymisaky@xingren.com>
2018-07-30 21:39:30 +0800

    修改格式

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const webpack = require('webpack');
-const path = require('path');
 
 /**
  * @type {webpack.Configuration}
  */
-const config =
-{
+const config = {
   entry: {
     app: './src/main.js',
     vendor: ['./src/lib/jquery.js', './src/lib/lodash.js'],
