9f88207f952b605d6d90437dc3810827820122a3
onlymisaky <onlymisaky@xingren.com>
2018-08-02 19:02:40 +0800

    loader..

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { str as loginStr } from './login';
 import { str as logoutStr } from './logout';
 
+import './styles/index.css';
+import './styles/index.less';
+import './styles/index.scss';
+
 const str = `我是入口文件，我加载了login.js和logout.js\n\r<br>${loginStr}\n\r<br>${logoutStr}`;
 
 document.body.innerHTML = str;
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,54 @@ const config = {
     path: __dirname + '/dist',
     filename: '[name].bundle.js',
     publicPath: '/assets/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: { presets: ['env'] },
+        include: __dirname + '/src'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   }
 }
 
